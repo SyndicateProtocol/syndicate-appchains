d1877711ac15065cd1f8cfc75d8a86377161e5cd
Daniil R <daniil@syndicate.io>
2024-10-02 09:51:59 -0700

    feat(proxy): proxy that forwards requests (#4)
    * feat(proxy): proxy that forwards requests
    - no headers
    - expose port 8456
    - split into 2 crates + make repo a workspace
    
    * fix(proxy): clean up
    
    * fix(proxy): please clippy

--- interceptor/src/main.rs
+++ interceptor/src/main.rs
@@ -1,18 +1,19 @@
 // Based on https://github.com/paritytech/jsonrpsee/blob/master/examples/examples/rpc_middleware_modify_request.rs
 
-use jsonrpsee::core::client::ClientT;
+use jsonrpsee::core::async_trait;
 use jsonrpsee::server::middleware::rpc::{RpcServiceBuilder, RpcServiceT};
-use jsonrpsee::server::Server;
+use jsonrpsee::server::{Server, ServerHandle};
 use jsonrpsee::types::Request;
-use jsonrpsee::ws_client::WsClientBuilder;
-use jsonrpsee::{rpc_params, RpcModule};
+use jsonrpsee::RpcModule;
 use std::borrow::Cow as StdCow;
 use std::net::SocketAddr;
+use tracing::info;
 use tracing_subscriber::EnvFilter;
 
 #[derive(Clone)]
 pub struct ModifyRequestIf<S>(S);
 
+#[async_trait]
 impl<'a, S> RpcServiceT<'a> for ModifyRequestIf<S>
 where
     S: Send + Sync + RpcServiceT<'a>,
@@ -20,6 +21,12 @@ where
     type Future = S::Future;
 
     fn call(&self, mut req: Request<'a>) -> Self::Future {
+        info!(
+            method = %req.method,
+            params = %req.params.as_ref().map(|r| r.get()).unwrap_or("None"),
+            "Received request"
+        );
+
         // Example how to modify the params in the call.
         // TODO: Change this to intercept eth_sendRawTransaction
         if req.method == "say_hello" {
@@ -48,22 +55,22 @@ async fn main() -> anyhow::Result<()> {
         .try_init()
         .expect("setting default subscriber failed");
 
-    let addr = run_server().await?;
-    let url = format!("ws://{}", addr);
+    let port = 8456;
+    let (addr, handle) = run_server(port).await?;
+    println!("RPC Server started on {}", addr);
 
-    let client = WsClientBuilder::default().build(&url).await?;
-    let _response: String = client.request("say_hello", rpc_params![]).await?;
-    let _response: Result<String, _> = client.request("unknown_method", rpc_params![]).await;
-    let _: String = client.request("say_hello", rpc_params![]).await?;
+    // Keep the server running
+    handle.stopped().await;
 
     Ok(())
 }
 
-async fn run_server() -> anyhow::Result<SocketAddr> {
+async fn run_server(port: u16) -> anyhow::Result<(SocketAddr, ServerHandle)> {
     let rpc_middleware = RpcServiceBuilder::new().layer_fn(ModifyRequestIf);
+    // .layer_fn(LoggingMiddleware);
     let server = Server::builder()
         .set_rpc_middleware(rpc_middleware)
-        .build("127.0.0.1:0")
+        .build(format!("127.0.0.1:{}", port))
         .await?;
     let mut module = RpcModule::new(());
     module.register_method("say_hello", |_, _, _| "lo")?;
@@ -74,7 +81,7 @@ async fn run_server() -> anyhow::Result<SocketAddr> {
 
     // In this example we don't care about doing shutdown so let's it run forever.
     // You may use the `ServerHandle` to shut it down or manage it yourself.
-    tokio::spawn(handle.stopped());
+    // tokio::spawn(handle.stopped());
 
-    Ok(addr)
+    Ok((addr, handle))
 }
