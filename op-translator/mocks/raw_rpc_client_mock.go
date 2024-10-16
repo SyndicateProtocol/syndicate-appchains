d4b87333039b69b2e9cd01f6af4b0cb45d96eae9
Eric Velazquez <ericvelmar95@gmail.com>
2024-10-16 21:05:13 +0200

    fix fmt

--- op-translator/mocks/raw_rpc_client_mock.go
+++ op-translator/mocks/raw_rpc_client_mock.go
@@ -7,18 +7,15 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-// MockRPCClient is a mock of *rpc.Client for testing
 type MockRawRPCClient struct {
 	mock.Mock
 }
 
-// Mocking CallContext method of *rpc.Client
-func (m *MockRawRPCClient) CallContext(ctx context.Context, result interface{}, method string, args ...interface{}) error {
+func (m *MockRawRPCClient) CallContext(ctx context.Context, result any, method string, args ...any) error {
 	arguments := m.Called(ctx, result, method, args)
 	return arguments.Error(0)
 }
 
-// Mock other methods like BatchCallContext as needed
 func (m *MockRawRPCClient) BatchCallContext(ctx context.Context, b []rpc.BatchElem) error {
 	arguments := m.Called(ctx, b)
 	return arguments.Error(0)
