d80559eee91ebe2a68b1d30b5459a8d599af88d5
Gus <gustavoguimaraes@users.noreply.github.com>
2024-10-16 21:36:30 +0200

    Feature/seq 105 fn for tx chunking (#14)
    * remove obsolete folder and file
    
    * get rid of reth-exex folder
    * organize contracts folder
    
    * remove obsolete files
    
    * get rid of reth-exexs
    * organize contracts folder
    
    * add fuc for tx chunking
    
    * fix syndicate frame integration test
    
    * fix deployment
    
    * remove extra .gitmodules file
    
    * add PR suggestions

--- metabased-contracts/script/DeployContractsForSequencerChain.s.sol
+++ metabased-contracts/script/DeployContractsForSequencerChain.s.sol
@@ -4,122 +4,39 @@ pragma solidity ^0.8.13;
 import {Script, console} from "forge-std/Script.sol";
 
 import {MetabasedSequencerChain} from "src/MetabasedSequencerChain.sol";
-import {SealedBidAuctionSequencingModule} from "src/sequencing-modules/SealedBidAuctionSequencingModule.sol";
-import {TokenBalanceSequencingModule} from "src/sequencing-modules/TokenBalanceSequencingModule.sol";
-import {AllowlistSequencingModule} from "src/sequencing-modules/AllowlistSequencingModule.sol";
 import {AlwaysAllowedModule} from "src/sequencing-modules/AlwaysAllowedModule.sol";
 
-contract DeployMetabasedSequencerChain is Script {
-    MetabasedSequencerChain public metabasedSequencerChainContract;
+import {MetabasedFactory} from "src/MetabasedFactory.sol";
+
+contract DeployMetabasedFactory is Script {
+    MetabasedFactory public metabasedFactory;
 
     uint256 public l3ChainId;
 
     function run() public {
         vm.startBroadcast();
+
         l3ChainId = 0; // TODO: Set the L3 chain ID
 
-        metabasedSequencerChainContract = new MetabasedSequencerChain(l3ChainId);
-        console.log("Deployed MetabasedSequencerChain", address(metabasedSequencerChainContract));
+        // metafiller  admin and manager
+        address admin = vm.envOr("ADMIN_ADDR", msg.sender);
+        address manager = vm.envOr("MANAGER_ADDR", msg.sender);
 
-        // Example on how to add/remove the modules to the requireAllList and/or requireAnyList of MetabasedSequencerChain
+        metabasedFactory = new MetabasedFactory();
+        console.log("Deployed MetabasedFactory", address(metabasedFactory));
 
-        // metabasedSequencerChainContract.addRequireAllCheck(address(sealedBidAuctionSequencingModule), true);
-        // metabasedSequencerChainContract.removeRequireAllCheck(address(sealedBidAuctionSequencingModule));
-
-        // metabasedSequencerChainContract.addRequireAnyCheck(address(tokenBalanceSequencingModule), true);
-        // metabasedSequencerChainContract.removeRequireAnyCheck(address(allowlistSequencingModule));
-
-        vm.stopBroadcast();
-    }
-}
-
-contract AddModuleToMetabasedSequencerChain is Script {
-    MetabasedSequencerChain public metabasedSequencerChainContract;
-
-    address public module;
-
-    function run() public {
-        vm.startBroadcast();
-        // MetabasedSequencerChain address on Base Sepolia
-        metabasedSequencerChainContract = MetabasedSequencerChain(0x73Ba7D784d13Ec0070a4Ea6F49Ff57dc007Bb48d);
-        // AlwaysAllowedModule address on Base Sepolia
-        module = 0x0324b4d8F786e11206F82e016DD4480de2332cF3;
-
-        metabasedSequencerChainContract.addRequireAllCheck(module, true);
-        console.log("Added module to MetabasedSequencerChain", module);
+        // create new MetabasedSequencerChain contracts
+        (address sequencerChain, address backfillMapper, address backfillStorage) =
+            metabasedFactory.createAllContracts(admin, manager, l3ChainId);
+        console.log("Deployed MetabasedSequencerChain", sequencerChain);
+        console.log("Deployed L3BackfillMapper", backfillMapper);
+        console.log("Deployed L3BackfillStorage", backfillStorage);
 
         vm.stopBroadcast();
     }
 }
 
-contract DeploySealedBidAuctionSequencingModule is Script {
-    SealedBidAuctionSequencingModule public sealedBidAuctionSequencingModule;
-
-    uint256 public auctionDuration;
-    address public treasury;
-
-    function run() public {
-        vm.startBroadcast();
-        auctionDuration = 1 days; // TODO: Set the auction duration
-        treasury = address(0); // TODO: Set the treasury address
-
-        sealedBidAuctionSequencingModule = new SealedBidAuctionSequencingModule(auctionDuration, treasury);
-        console.log("Deployed SealedBidAuctionSequencingModule", address(sealedBidAuctionSequencingModule));
-
-        vm.stopBroadcast();
-    }
-}
-
-contract DeployTokenBalanceSequencingModule is Script {
-    TokenBalanceSequencingModule public tokenBalanceSequencingModule;
-
-    address public token;
-    uint256 public minimumBalance;
-
-    function run() public {
-        vm.startBroadcast();
-
-        token = address(0); // TODO: Set the token address
-        minimumBalance = 0; // TODO: Set the minimum balance
-
-        tokenBalanceSequencingModule = new TokenBalanceSequencingModule(token, minimumBalance);
-        console.log("Deployed TokenBalanceSequencingModule", address(tokenBalanceSequencingModule));
-
-        vm.stopBroadcast();
-    }
-}
-
-contract DeployAllowlistSequencingModule is Script {
-    AllowlistSequencingModule public allowlistSequencingModule;
-
-    address public admin;
-
-    function run() public {
-        vm.startBroadcast();
-
-        admin = 0x45d6450fC59A6F8D9c753126cE1EFF6fa4D7e0fb; // deployer address
-
-        allowlistSequencingModule = new AllowlistSequencingModule(admin);
-        console.log("Deployed AllowlistSequencingModule", address(allowlistSequencingModule));
-
-        vm.stopBroadcast();
-    }
-}
-
-contract DeployAlwaysAllowedModule is Script {
-    AlwaysAllowedModule public alwaysAllowedModule;
-
-    function run() public {
-        vm.startBroadcast();
-
-        alwaysAllowedModule = new AlwaysAllowedModule();
-        console.log("Deployed AlwaysAllowedModule", address(alwaysAllowedModule));
-
-        vm.stopBroadcast();
-    }
-}
-
-contract DeployMetabasedSequencerChainForTestnet is Script {
+contract DeployMetabasedSequencerChainPlusSetupWithAlwaysAllowModule is Script {
     MetabasedSequencerChain public metabasedSequencerChainContract;
 
     uint256 public l3ChainId;
