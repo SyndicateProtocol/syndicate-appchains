d52f515c61b01fcb14c90930c16ba9ebd46bd7ee
Roman Hodulák <roman@syndicate.io>
2024-11-04 11:37:24 +0100

    refactor(sequencer): Measure latency from the presentation layer [SEQ-211] (#49)
    - Avoids passing dependencies that are only needed for metrics
    - Provides more accurate measurements because it includes the time outside of the application scenario

--- metabased-sequencer/interceptor/src/application/send_raw_transaction.rs
+++ metabased-sequencer/interceptor/src/application/send_raw_transaction.rs
@@ -1,4 +1,3 @@
-use crate::application::{Metrics, RunningStopwatch};
 use crate::domain::primitives::Bytes;
 use crate::domain::MetabasedSequencerChainService;
 use crate::presentation::json_rpc_errors::Error;
@@ -11,44 +10,30 @@ use alloy::primitives::TxHash;
 use alloy::primitives::U256;
 
 /// Sends serialized and signed transaction `tx` using `chain`.
-pub async fn send_raw_transaction<Chain, M: Metrics, S: RunningStopwatch>(
-    encoded: Bytes,
-    chain: &Chain,
-    metrics: &M,
-    start: &S,
-) -> Result<TxHash, Error>
+pub async fn send_raw_transaction<Chain>(encoded: Bytes, chain: &Chain) -> Result<TxHash, Error>
 where
     Chain: MetabasedSequencerChainService,
     Error: From<<Chain as MetabasedSequencerChainService>::Error>,
 {
-    let result = async move {
-        // 1. Decoding:
-        let mut slice: &[u8] = encoded.as_ref();
-        let tx = TxEnvelope::decode(&mut slice)?;
+    // 1. Decoding:
+    let mut slice: &[u8] = encoded.as_ref();
+    let tx = TxEnvelope::decode(&mut slice)?;
 
-        // 2. Validation:
-        tx.recover_signer()?;
+    // 2. Validation:
+    tx.recover_signer()?;
 
-        if tx.tx_type() == TxType::Legacy {
-            // TODO(SEQ-179): introduce optional global tx cap config. See op-geth's checkTxFee() + RPCTxFeeCap for equivalent
-            // skip check if unset
-            let tx_cap_in_wei = U256::from(1_000_000_000_000_000_000u64); // 1e18wei = 1 ETH
-            let gas_price = tx.gas_price().ok_or(InvalidInput(MissingGasPrice))?;
-            transaction::check_tx_fee(
-                U256::try_from(gas_price)?,
-                U256::try_from(tx.gas_limit())?,
-                tx_cap_in_wei,
-            )?;
-        }
-
-        // 3. Submission/forwarding:
-        Ok(chain.process_transaction(encoded).await?)
+    if tx.tx_type() == TxType::Legacy {
+        // TODO(SEQ-179): introduce optional global tx cap config. See op-geth's checkTxFee() + RPCTxFeeCap for equivalent
+        // skip check if unset
+        let tx_cap_in_wei = U256::from(1_000_000_000_000_000_000u64); // 1e18wei = 1 ETH
+        let gas_price = tx.gas_price().ok_or(InvalidInput(MissingGasPrice))?;
+        transaction::check_tx_fee(
+            U256::try_from(gas_price)?,
+            U256::try_from(tx.gas_limit())?,
+            tx_cap_in_wei,
+        )?;
     }
-    .await;
 
-    let elapsed = start.elapsed();
-
-    metrics.append_send_raw_transaction_with_duration(elapsed);
-
-    result
+    // 3. Submission/forwarding:
+    Ok(chain.process_transaction(encoded).await?)
 }
