4c2e9612d2ef716bbc50035147b458f3dee0e6ef
Eric Velazquez <ericvelmar95@gmail.com>
2024-11-05 15:13:04 +0700

    scripts: add send_l3_txn_script

---
+++ ops/scripts/README.md
@@ -0,0 +1,6 @@
+# Metabased Rollup Scripts
+
+This folder contains various scripts and tools to assist in testing and interacting with our rollup system. These scripts are designed to streamline common tasks, automate processes, and provide utilities for development and debugging.
+
+## Scripts
+- send_l3_txn: This script facilitates the process of creating and sending a Layer 3 transaction to a specified sequencing contract on Layer 2.
---
+++ ops/scripts/send_l3_txn.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"context"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"math/big"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load environment variables from .env file
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	// Get environment variables
+	sequencingRPCUrl := os.Getenv("SEQUENCING_RPC_URL")
+	sequencingContractAddress := common.HexToAddress(os.Getenv("SEQUENCING_CONTRACT_ADDRESS"))
+	l3ChainIDString := os.Getenv("L3_CHAIN_ID")
+	sequencingChainIDString := os.Getenv("SEQUENCING_CHAIN_ID")
+	l3PrivateKey := os.Getenv("PRIVATE_KEY_L3")
+	stratosPrivateKey := os.Getenv("PRIVATE_KEY_STRATOS")
+
+	// Define the RPC URL and connect to the client
+	client, err := ethclient.Dial(sequencingRPCUrl)
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+
+	// Step 1: Define and sign the raw transaction to be passed as input data
+	// Parse chain IDs from environment variables
+	l3ChainID, err := strconv.ParseInt(l3ChainIDString, 10, 64)
+	if err != nil {
+		log.Fatalf("Failed to parse l3ChainIDString: %v", err)
+	}
+	sequencingChainID, err := strconv.ParseInt(sequencingChainIDString, 10, 64)
+	if err != nil {
+		log.Fatalf("Failed to parse sequencingChainIDString: %v", err)
+	}
+
+	// L3 Transaction parameters
+	l3ChainIDBig := big.NewInt(l3ChainID)
+	toAddress := common.HexToAddress("0x52A4380F691E71ff0015352AB1a450a1dfb689b9")
+
+	// Load the private key for signing from environment variable
+	privateKeyL3, err := crypto.HexToECDSA(l3PrivateKey[2:])
+	if err != nil {
+		log.Fatalf("Invalid private key: %v", err)
+	}
+
+	// Create a new L3 raw transaction
+	rawTx := types.NewTx(&types.DynamicFeeTx{
+		ChainID:   l3ChainIDBig,
+		Nonce:     uint64(3),
+		To:        &toAddress,
+		Value:     big.NewInt(1e18),
+		Gas:       uint64(50000), // gasLimit
+		Data:      []byte{},
+		GasFeeCap: big.NewInt(10e9), // maxFeePerGas
+		GasTipCap: big.NewInt(10e9), // maxPriorityFeePerGas
+	})
+
+	// Sign the L3 raw transaction
+	signedRawTx, err := types.SignTx(rawTx, types.NewLondonSigner(l3ChainIDBig), privateKeyL3)
+	if err != nil {
+		log.Fatalf("Failed to sign raw transaction: %v", err)
+	}
+
+	// Encode the signed L3 transaction as input for the sequencing contract
+	signedTxData, err := signedRawTx.MarshalBinary()
+	if err != nil {
+		log.Fatalf("Failed to encode signed transaction: %v", err)
+	}
+
+	// ABI encode the function call with the signed transaction as input
+	contractABI, err := abi.JSON(strings.NewReader(`[{"inputs":[{"internalType":"bytes","name":"transaction","type":"bytes"}],"name":"processTransaction","outputs":[],"stateMutability":"nonpayable","type":"function"}]`))
+	if err != nil {
+		log.Fatalf("Failed to parse ABI: %v", err)
+	}
+
+	// Pack the `processTransaction` function call with the signed transaction data
+	inputData, err := contractABI.Pack("processTransaction", signedTxData)
+	if err != nil {
+		log.Fatalf("Failed to pack function call: %v", err)
+	}
+
+	// Step 3: Send the L2 transaction to the sequencing contract
+	privateKeyStratos, err := crypto.HexToECDSA(stratosPrivateKey[2:])
+	if err != nil {
+		log.Fatalf("Invalid private key: %v", err)
+	}
+	auth, err := bind.NewKeyedTransactorWithChainID(privateKeyStratos, big.NewInt(sequencingChainID))
+	if err != nil {
+		log.Fatalf("Failed to create transactor: %v", err)
+	}
+
+	// Send the transaction with the packed data to the sequencing contract
+	gasPriceL2 := big.NewInt(10e9)
+	gasLimitL2 := uint64(5000000)
+	nonceL2 := uint64(1)
+	tx := types.NewTransaction(nonceL2, sequencingContractAddress, big.NewInt(0), gasLimitL2, gasPriceL2, inputData)
+	signedTx, err := auth.Signer(auth.From, tx)
+	log.Printf("Signed tx data: %s", hex.EncodeToString(signedTxData))
+	if err != nil {
+		log.Fatalf("Failed to sign transaction: %v", err)
+	}
+
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		log.Fatalf("Failed to send transaction: %v", err)
+	}
+
+	fmt.Printf("Transaction sent to contract! Hash: %s\n", signedTx.Hash().Hex())
+}
