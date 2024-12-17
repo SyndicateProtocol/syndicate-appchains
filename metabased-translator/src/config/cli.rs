a636d4207326a401f24aa1e41b1a716e2e4707b4
Daniil R <daniil@syndicate.io>
2024-12-17 11:47:47 -0800

    feat: run anvil as server (#117)
    - refactored to `anvil.rs`
    - enabled logging
    - run me with env var `RUST_LOG=debug` or `RUST_LOG=info`

---
+++ metabased-translator/src/config/cli.rs
@@ -0,0 +1,9 @@
+use tracing_subscriber::EnvFilter;
+
+// Sets global formatter subscriber, including env variables
+pub fn init_tracing_subscriber() {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .try_init()
+        .expect("setting default subscriber failed");
+}
