889152d109ab9934ffb73be7eb1b0591106fc930
Daniil Ruditskiy <daniil@syndicate.io>
2024-12-16 16:29:22 -0800

    test: anvil set code works

--- metabased-translator/src/main.rs
+++ metabased-translator/src/main.rs
@@ -2,6 +2,7 @@ use eyre::Result;
 use metabased_translator::config::cli;
 use metabased_translator::connectors::anvil;
 
+// PoC deploying a contract using `anvil_set_code`, then interacting with it
 #[tokio::main]
 async fn main() -> Result<()> {
     cli::init_tracing_subscriber();
