0638a232e514b4993257a5bae0d94e875cd317e7
Daniil Ruditskiy <daniil@syndicate.io>
2024-12-18 13:27:00 -0800

    fix: fixed test

--- metabased-translator/src/connectors/anvil.rs
+++ metabased-translator/src/connectors/anvil.rs
@@ -72,7 +72,6 @@ pub async fn run() -> eyre::Result<()> {
     provider.anvil_set_code(address, bytecode.clone().into()).await?;
     info!("Deployed contract code at: {}", address);
 
-    // create a map of addresses to bytecode
     deploy_contracts(server_url.clone()).await?;
 
     // // Create contract instance
@@ -215,7 +214,7 @@ mod tests {
         }
     }
 
-    // Your existing Counter contract definition
+    // Existing simple Counter contract definition
     sol! {
         #[allow(missing_docs)]
         #[sol(rpc, bytecode="6080806040523460135760df908160198239f35b600080fdfe6080806040526004361015601257600080fd5b60003560e01c9081633fb5c1cb1460925781638381f58a146079575063d09de08a14603c57600080fd5b3460745760003660031901126074576000546000198114605e57600101600055005b634e487b7160e01b600052601160045260246000fd5b600080fd5b3460745760003660031901126074576020906000548152f35b34607457602036600319011260745760043560005500fea2646970667358221220e978270883b7baed10810c4079c941512e93a7ba1cd1108c781d4bc738d9090564736f6c634300081a0033")]
@@ -265,7 +264,7 @@ mod tests {
         // Verify the transaction succeeded
         assert!(receipt.status());
 
-        // Uncomment to see logs
+        // Uncomment me to see logs
         // receipt.inner.logs().iter().for_each(|log| {
         //     println!("Log: {:?}", log);
         // });
