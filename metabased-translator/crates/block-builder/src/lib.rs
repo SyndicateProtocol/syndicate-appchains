13a0d0c00bb5c20506c7705d0ac639ff454d702b
Jorge Silva <jorge@syndicate.io>
2024-12-20 12:26:32 -0800

    fix contract bindings

--- metabased-translator/crates/block-builder/src/lib.rs
+++ metabased-translator/crates/block-builder/src/lib.rs
@@ -1,8 +1,5 @@
 pub mod config;
 pub mod connectors;
-<<<<<<< HEAD:metabased-translator/crates/block-builder/src/lib.rs
 #[rustfmt::skip]
-=======
->>>>>>> ac36494 (WIP):metabased-translator/src/lib.rs
 pub mod contract_bindings;
 pub mod rollups;
