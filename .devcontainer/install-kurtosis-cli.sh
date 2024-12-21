c7e54146aae2c45db5d5d4ca80e38bc50acaec71
Will Papper <contact@papper.me>
2024-12-20 17:46:55 -0800

    fix: Run Kurtosis setup command in zsh

--- .devcontainer/install-kurtosis-cli.sh
+++ .devcontainer/install-kurtosis-cli.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # Based on https://docs.kurtosis.com/install/#ii-install-the-cli
 echo "deb [trusted=yes] https://apt.fury.io/kurtosis-tech/ /" | sudo tee /etc/apt/sources.list.d/kurtosis.list
 sudo apt update
