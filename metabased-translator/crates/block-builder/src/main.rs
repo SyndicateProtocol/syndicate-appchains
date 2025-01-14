fde702317985d5730a3df053b69fc2f697c58d7b
Isaac <isaac@syndicate.io>
2025-01-14 10:08:15 -0500

    Initial skeleton of block building flow with Arb

--- metabased-translator/crates/block-builder/src/main.rs
+++ metabased-translator/crates/block-builder/src/main.rs
@@ -2,19 +2,44 @@
 
 use block_builder::config::{cli, Configuration};
 use block_builder::connectors::anvil::MetaChainProvider;
+use block_builder::rollups::arbitrum::arbitrum_builder::ArbitrumBlockBuilder;
+use block_builder::rollups::utils::sequencing_chain_blocks_to_mbtxs;
 use eyre::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // Initialize Block Builder
     cli::init_tracing_subscriber();
     let config = Configuration::parse();
 
+    // Initialize MChain
     let mchain = MetaChainProvider::start(config).await?;
+    // TODO: Setup MChain if we are starting from scratch
+    //       - Deploy contracts
+    //       - Fund accounts
 
-    mchain.mine_block().await?;
+    // Initialize block builder
+    let arb = ArbitrumBlockBuilder::new();
 
     //loop
     loop {
+        // TODO: Consume Blob of blocks from ingestor
+        let blocks = vec![];
+
+        // Process sequencing chain blocks into mB transactions
+        let mbtxs = sequencing_chain_blocks_to_mbtxs(blocks);
+
+        // TODO: [OP / ARB] Process deposit transactions
+
+        // [OP / ARB] Build batch
+        let batch_txn = arb.build_batch_txn(mbtxs);
+
+        // Submit batch transaction to mchain
+        mchain.submit_txn(batch_txn).await?;
+
+        // Mine mchain block
+        mchain.mine_block().await?;
+
         tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
     }
 }
