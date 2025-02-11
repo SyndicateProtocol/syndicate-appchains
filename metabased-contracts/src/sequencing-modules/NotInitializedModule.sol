2c4fa714116bb6b46ff7fbabbd5a9a3ea4a626f8
caleb <caleb@syndicate.io>
2025-02-11 13:26:21 -0600

    comments

--- metabased-contracts/src/sequencing-modules/NotInitializedModule.sol
+++ metabased-contracts/src/sequencing-modules/NotInitializedModule.sol
@@ -3,6 +3,8 @@ pragma solidity 0.8.25;
 
 import {PermissionModule} from "../interfaces/PermissionModule.sol";
 
+/// @title NotInitializedModule
+/// @notice A module that returns false for all addresses
 contract NotInitializedModule is PermissionModule {
     /// @notice Returns false for all addresses
     function isAllowed(address) external pure override returns (bool) {
