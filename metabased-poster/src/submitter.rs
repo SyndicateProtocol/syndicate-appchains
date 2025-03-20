d6a3a9ba61ef80a13c9721c0189ec4769bc36902
Eric Velazquez <ericvelmar95@gmail.com>
2025-03-20 15:04:09 +0000

    update submitter

--- metabased-poster/src/submitter.rs
+++ metabased-poster/src/submitter.rs
@@ -16,10 +16,10 @@ use alloy::{
 use contract_bindings::arbitrum::iassertionposter::IAssertionPoster::{
     self, IAssertionPosterInstance,
 };
-use eyre::{eyre, Error};
+use eyre::{eyre, Result};
 use std::{str::FromStr, sync::Arc};
 use tokio::sync::{mpsc::Receiver, oneshot};
-use tracing::info;
+use tracing::{error, info};
 use url::Url;
 
 type FilledProvider = FillProvider<
@@ -45,10 +45,10 @@ pub async fn run(
     assertion_poster_contract_address: Address,
     blocks_rx: Receiver<Arc<NitroBlock>>,
     shutdown_rx: oneshot::Receiver<()>,
-) -> Result<(), Error> {
-    let signer = PrivateKeySigner::from_str(&private_key)
+) -> Result<()> {
+    let priv_k = PrivateKeySigner::from_str(&private_key)
         .unwrap_or_else(|err| panic!("Failed to parse default private key for signer: {}", err));
-    let provider = ProviderBuilder::new().wallet(EthereumWallet::from(signer)).on_http(rpc_url);
+    let provider = ProviderBuilder::new().wallet(EthereumWallet::from(priv_k)).on_http(rpc_url);
     let assertion_poster = IAssertionPoster::new(assertion_poster_contract_address, provider);
     let submitter = Submitter { assertion_poster };
     submitter.main_loop(blocks_rx, shutdown_rx).await
@@ -59,13 +59,15 @@ impl Submitter {
         &self,
         mut blocks_rx: Receiver<Arc<NitroBlock>>,
         mut shutdown_rx: oneshot::Receiver<()>,
-    ) -> Result<(), Error> {
+    ) -> Result<()> {
         info!("Starting Submitter...");
         loop {
             tokio::select! {
                 Some(block) = blocks_rx.recv() => {
-                    self.post_assertion(block).await;
-                },
+                   if let Err(err) = self.post_assertion(block).await {
+                    error!("Error submitting assertion: {:?}", err);
+                   }
+                }
                 _ = &mut shutdown_rx => {
                     return Err(eyre!("Shut down"))
                 }
@@ -73,8 +75,9 @@ impl Submitter {
         }
     }
 
-    async fn post_assertion(&self, block: Arc<NitroBlock>) {
-        let _ = self.assertion_poster.postAssertion(block.hash, block.send_root).send().await;
-        info!("Assertion submitted for block: {:?}", block);
+    async fn post_assertion(&self, block: Arc<NitroBlock>) -> Result<()> {
+        let _ = self.assertion_poster.postAssertion(block.hash, block.send_root).send().await?;
+        info!("Assertion submitted2 for block: {:?}", block);
+        Ok(())
     }
 }
