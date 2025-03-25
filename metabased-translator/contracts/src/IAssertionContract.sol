ffd37d6fb77d2dac279dfcedaca88d60e0375f55
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2025-03-25 19:18:16 +0100

    fix compiler issues

--- metabased-translator/contracts/src/IAssertionContract.sol
+++ metabased-translator/contracts/src/IAssertionContract.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.8.25;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
 
 interface IAssertionPoster {
     function postAssertion(bytes32 blockHash, bytes32 sendRoot) external;
