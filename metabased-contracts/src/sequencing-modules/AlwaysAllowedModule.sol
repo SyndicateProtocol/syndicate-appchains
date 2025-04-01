b29f138112b8ec16e102c36d11d2124ca2e7f2b6
ibremseth <isaac@syndicate.io>
2025-04-01 09:48:39 -0400

    Update contract to fix tests (#380)
    * Update contract to fix tests
    
    * fix binding

--- metabased-contracts/src/sequencing-modules/AlwaysAllowedModule.sol
+++ metabased-contracts/src/sequencing-modules/AlwaysAllowedModule.sol
@@ -2,17 +2,22 @@
 pragma solidity 0.8.25;
 
 import {ProposerPermissionModule} from "../interfaces/ProposerPermissionModule.sol";
+import {CalldataPermissionModule} from "../interfaces/CalldataPermissionModule.sol";
 
 /**
  * @title AlwaysAllowedModule
  * @dev Module used for testing and experimentations. It allows any proposer to send batch data.
  */
-contract AlwaysAllowedModule is ProposerPermissionModule {
+contract AlwaysAllowedModule is ProposerPermissionModule, CalldataPermissionModule {
     /**
      * @notice caller is allowed.
      * @return bool indicating if the caller is allowed.
      */
     function isAllowed(address) external pure override returns (bool) {
+        return true;
+    }
+
+    function isCalldataAllowed(bytes calldata) external pure override returns (bool) {
         return true;
     }
 }
