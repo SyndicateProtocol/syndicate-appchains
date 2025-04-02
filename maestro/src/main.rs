663837f16186ad1fca135407c3648b5bcb5db4dc
Daniil Ruditskiy <daniil@syndicate.io>
2025-04-01 19:51:42 -0700

    feat: add basic redis connection

--- maestro/src/main.rs
+++ maestro/src/main.rs
@@ -1,7 +1,8 @@
 //! Maestro is a service that filters and coordinates transaction requests to sequencers
 
 use eyre::Result;
-use maestro::config::Config;
+use maestro::{config::Config, errors::Error};
+use redis::{aio::MultiplexedConnection, AsyncCommands, Client};
 use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
 
@@ -23,9 +24,29 @@ async fn main() -> Result<()> {
         "Maestro server running"
     );
 
+    let (_redis_client, _redis_conn) = connect(config).await?;
+
+    info!("Connected to Redis successfully!");
+
     // Keep the server running
     tokio::select! {
         _ = handle.stopped() => {}
     }
     Ok(())
+}
+
+async fn connect(config: Config) -> Result<(Client, MultiplexedConnection), Error> {
+    info!("Connecting to Redis server at {}...", config.redis_address);
+    let client = Client::open(config.redis_address)?;
+    info!("Got Redis client");
+    let mut conn = client.get_multiplexed_async_connection().await?;
+    info!("Got Redis connection");
+
+    // Test an operation on Redis
+    conn.set::<&str, &str, ()>("test_key", "test_value").await?;
+
+    let value: String = conn.get("test_key").await?;
+
+    info!("Redis test value set: {}", value);
+    Ok((client, conn))
 }
