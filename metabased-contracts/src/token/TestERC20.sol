b1143ba77d760b786127e96f0b326031fe654185
caleb <caleb@syndicate.io>
2025-04-16 13:34:53 -0500

    test erc20

---
+++ metabased-contracts/src/token/TestERC20.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.25;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestERC20 is ERC20 {
+    constructor() ERC20("TestERC20", "TST20") {}
+
+    function mint(address to, uint256 amount) public {
+        require(amount != 0, "Amount cannot be zero");
+        _mint(to, amount);
+    }
+}
