eb4946a298148d1c686f65f1f0883c9daf2b87fe
caleb <caleb@syndicate.io>
2025-05-12 15:22:32 -0500

    update binding import

--- test-framework/src/components/test_components.rs
+++ test-framework/src/components/test_components.rs
@@ -75,7 +75,7 @@ pub struct TestComponents {
     /// Sequencing
     pub sequencing_provider: FilledProvider,
     pub sequencing_rpc_url: String,
-    pub sequencing_contract: MetabasedSequencerChainInstance<(), FilledProvider>,
+    pub sequencing_contract: SyndicateSequencerChainInstance<(), FilledProvider>,
 
     /// Settlement
     pub settlement_provider: FilledProvider,
@@ -130,7 +130,7 @@ impl TestComponents {
         // Launch mock sequencing chain and deploy contracts
         info!("Starting sequencing chain...");
         let (seq_port, seq_anvil, seq_provider) = start_anvil(15).await?;
-        _ = MetabasedSequencerChain::deploy_builder(
+        _ = SyndicateSequencerChain::deploy_builder(
             &seq_provider,
             U256::from(options.appchain_chain_id),
         )
@@ -148,7 +148,7 @@ impl TestComponents {
         // Setup the sequencing contract
         let provider_clone = seq_provider.clone();
         let sequencing_contract =
-            MetabasedSequencerChain::new(sequencing_contract_address, provider_clone);
+            SyndicateSequencerChain::new(sequencing_contract_address, provider_clone);
         _ = sequencing_contract
             .initialize(seq_provider.default_signer_address(), always_allowed_module_address)
             .send()
