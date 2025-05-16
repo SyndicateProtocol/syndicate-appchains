4999f5dc1ca74cba3377716bcb7d36ba86494170
tsite <stubydent@gmail.com>
2025-05-16 16:46:45 -0700

    add TeeModule.sol contract

---
+++ shared/arb-contracts/src/TeeModule.sol
@@ -0,0 +1,175 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+import {AssertionPoster} from "./AssertionPoster.sol";
+
+import {Ownable} from "@openzeppelin/contracts/ownership/Ownable.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+
+using ECDSA for bytes32;
+using MessageHashUtils for bytes32;
+
+interface IL1Block {
+    function timestamp() external view returns (uint256);
+    function hash() external view returns (bytes32);
+}
+
+struct TeeTrustedInput {
+    bytes32 appchainConfigHash;
+    bytes32 setEndBlockHash;
+    bytes32 l1EndBlockHash;
+    bytes32 appchainStartBlockHash;
+}
+
+struct PendingAssertion {
+    bytes32 blockHash;
+    bytes32 sendRoot;
+}
+
+struct AssertionPayload {
+    bytes32 teeTrustedInputHash;
+    PendingAssertion assertion;
+}
+
+function hash_input(TeeTrustedInput storage a) view returns (bytes32) {
+    return
+        keccak256(abi.encodePacked(a.appchainConfigHash, a.setEndBlockHash, a.l1EndBlockHash, a.appchainStartBlockHash));
+}
+
+function equals(PendingAssertion calldata a, PendingAssertion storage b) view returns (bool) {
+    return a.blockHash == b.blockHash && a.sendRoot == b.sendRoot;
+}
+
+event TeeKeysRevoked();
+
+event TeeProgramHashAdded(bytes32 hash);
+
+event TeeProgramHashRemoved(bytes32 hash);
+
+event TeeAppchainConfigHash(bytes32 hash);
+
+event TeeHacked(uint256);
+
+event ChallengeResolved(PendingAssertion);
+
+/**
+ * @title TeeModule Contract
+ */
+contract TeeModule is Ownable {
+    // Immutable state variables
+    AssertionPoster public immutable poster;
+
+    // TEE variables
+    TeeTrustedInput public teeTrustedInput;
+    PendingAssertion[] public pendingAssertions;
+    uint256 public teeHackCount;
+    address[] public teeKeys;
+    mapping(address => bool) public isTeeKey;
+    mapping(bytes32 => bool) public isTeeProgramHash;
+    uint256 public challengeWindowEnd;
+    uint256 public challengeWindowDuration;
+
+    /**
+     * @notice Constructs the AssertionPoster contract
+     * @param poster_ Address of the assertion poster contract
+     * @param appchainConfigHash_ Hash of the appchain configuration data passed to the TEE
+     * Note that the AssertionPoster must be owned by the TeeModule for closing the challenge window to work properly
+     */
+    constructor(AssertionPoster poster_, bytes32 appchainConfigHash_) {
+        poster = poster_;
+        teeTrustedInput.appchainConfigHash = appchainConfigHash_;
+        emit TeeAppchainConfigHash(appchainConfigHash_);
+    }
+
+    function closeChallengeWindow() public {
+        IL1Block l1block = IL1Block(address(0x4200000000000000000000000000000000000015));
+        require(pendingAssertions.length <= 1, "cannot close challenge window - too many assertions");
+        require(
+            l1block.timestamp() > challengeWindowEnd, "cannot close challenge window - insufficient time has passed"
+        );
+        if (pendingAssertions.length > 0) {
+            teeTrustedInput.appchainStartBlockHash = pendingAssertions[0].blockHash;
+            poster.postAssertion(pendingAssertions[0].blockHash, pendingAssertions[0].sendRoot);
+        }
+        teeTrustedInput.setEndBlockHash = blockhash(block.number - 1);
+        teeTrustedInput.l1EndBlockHash = l1block.hash();
+        challengeWindowEnd = block.timestamp + challengeWindowDuration;
+    }
+
+    function submitAssertion(AssertionPayload calldata payload, bytes calldata signature, address rewardAddr)
+        external
+    {
+        require(signature.length == 65, "invalid signature length");
+        require(payload.teeTrustedInputHash == hash_input(teeTrustedInput), "invalid tee input");
+        bytes32 payload_hash = keccak256(
+            abi.encodePacked(payload.teeTrustedInputHash, payload.assertion.blockHash, payload.assertion.sendRoot)
+        );
+        require(isTeeKey[payload_hash.toEthSignedMessageHash().recover(signature)], "invalid tee signature");
+        for (uint256 i = 0; i < pendingAssertions.length; i++) {
+            require(!equals(payload.assertion, pendingAssertions[i]), "assertion already exists");
+        }
+        if (pendingAssertions.length == 0) {
+            challengeWindowEnd = block.timestamp + challengeWindowDuration;
+        }
+        pendingAssertions.push(payload.assertion);
+        if (pendingAssertions.length == 2) {
+            teeHackCount += 1;
+            emit TeeHacked(teeHackCount);
+            // pay out rewards
+            (bool success,) = payable(rewardAddr).call{value: address(this).balance}("");
+            require(success, "payment failed");
+        }
+    }
+
+    function setChallengeWindowDuration(uint256 duration) external onlyOwner {
+        challengeWindowDuration = duration;
+    }
+
+    function resolveChallenge(PendingAssertion calldata assertion) external onlyOwner {
+        require(pendingAssertions.length > 1, "challenge does not exist");
+        for (uint256 i = 0; i < pendingAssertions.length; i++) {
+            if (equals(assertion, pendingAssertions[i])) {
+                delete pendingAssertions;
+                pendingAssertions.push(assertion);
+                challengeWindowEnd = 0;
+                closeChallengeWindow();
+                emit ChallengeResolved(assertion);
+                return;
+            }
+        }
+        revert("assertion not found");
+    }
+
+    function addTeeKey(address publicKey, bytes calldata zkProof) external {
+        // todo: validate the zk proof
+        assert(zkProof.length == 1);
+        if (!isTeeKey[publicKey]) {
+            isTeeKey[publicKey] = true;
+            teeKeys.push(publicKey);
+        }
+    }
+
+    function revokeAllTeeKeys() external onlyOwner {
+        for (uint256 i = 0; i < teeKeys.length; i++) {
+            isTeeKey[teeKeys[i]] = false;
+        }
+        delete teeKeys;
+        emit TeeKeysRevoked();
+    }
+
+    function addTeeProgramHash(bytes32 hash) external onlyOwner {
+        isTeeProgramHash[hash] = true;
+        emit TeeProgramHashAdded(hash);
+    }
+
+    function removeTeeProgramHash(bytes32 hash) external onlyOwner {
+        isTeeProgramHash[hash] = false;
+        emit TeeProgramHashRemoved(hash);
+    }
+
+    function setAppchainConfigHash(bytes32 hash) external onlyOwner {
+        teeTrustedInput.appchainConfigHash = hash;
+        emit TeeAppchainConfigHash(hash);
+    }
+}
