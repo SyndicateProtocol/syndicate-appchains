b98b08259039e5cc9f07f8735a9f1969df496157
tsite <stubydent@gmail.com>
2025-05-21 23:16:26 -0700

    update tee module

--- shared/arb-contracts/src/TeeModule.sol
+++ shared/arb-contracts/src/TeeModule.sol
@@ -7,30 +7,29 @@ import {Ownable} from "@openzeppelin/contracts/ownership/Ownable.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
+import "@arbitrum/nitro-contracts/src/bridge/IBridge.sol";
+
 using ECDSA for bytes32;
 using MessageHashUtils for bytes32;
 
 interface IL1Block {
-    function number() external view returns (uint64);
     function timestamp() external view returns (uint64);
     function hash() external view returns (bytes32);
 }
+
+IL1Block constant l1block = IL1Block(0x4200000000000000000000000000000000000015);
 
 struct TeeTrustedInput {
     // appchain - requires a full node
     bytes32 appchainConfigHash;
     bytes32 appchainStartBlockHash;
-    bytes32 appchainDelayedMessagesHash;
     // sequencing chain - requires a full node
     bytes32 seqConfigHash;
     bytes32 seqStartBlockHash;
-    bytes32 seqDelayedMessagesHash;
-    // settlement & l1 chains - do not require full nodes
-    uint64 setStartBlockNumber;
-    uint64 setEndBlockNumber;
-    uint64 l1StartBlockNumber;
-    uint64 l1EndBlockNumber;
-    bytes32 setEndBlockHash;
+    // settlement chain - does not require a full node
+    bytes32 setDelayedMessageAcc;
+    // l1 chain - does not require a full node
+    bytes32 l1StartBlockHash;
     bytes32 l1EndBlockHash;
 }
 
@@ -38,10 +37,8 @@ struct PendingAssertion {
     // appchain
     bytes32 blockHash;
     bytes32 sendRoot;
-    bytes32 delayedMessagesHash;
     // sequencing chain
     bytes32 seqBlockHash;
-    bytes32 seqDelayedMessagesHash;
 }
 
 function hash_input(TeeTrustedInput storage a) view returns (bytes32) {
@@ -49,23 +46,17 @@ function hash_input(TeeTrustedInput storage a) view returns (bytes32) {
         abi.encodePacked(
             a.appchainConfigHash,
             a.appchainStartBlockHash,
-            a.appchainDelayedMessagesHash,
             a.seqConfigHash,
             a.seqStartBlockHash,
-            a.seqDelayedMessagesHash,
-            a.setStartBlockNumber,
-            a.setEndBlockNumber,
-            a.setEndBlockHash,
-            a.l1StartBlockNumber,
-            a.l1EndBlockNumber,
+            a.setDelayedMessageAcc,
+            a.l1StartBlockHash,
             a.l1EndBlockHash
         )
     );
 }
 
 function equals(PendingAssertion calldata a, PendingAssertion storage b) view returns (bool) {
-    return a.blockHash == b.blockHash && a.sendRoot == b.sendRoot && a.delayedMessagesHash == b.delayedMessagesHash
-        && a.seqBlockHash == b.seqBlockHash && a.seqDelayedMessagesHash == b.seqDelayedMessagesHash;
+    return a.blockHash == b.blockHash && a.sendRoot == b.sendRoot && a.seqBlockHash == b.seqBlockHash;
 }
 
 event TeeKeysRevoked();
@@ -74,9 +65,9 @@ event TeeProgramAdded(bytes32 hash);
 
 event TeeProgramRemoved(bytes32 hash);
 
-event TeeAppchainConfigHash(bytes32 hash);
+event TeeAppchainConfigHash(bytes32 configHash, bytes32 blockHash);
 
-event TeeSeqConfigHash(bytes32 hash);
+event TeeSeqConfigHash(bytes32 configHash, bytes32 blockHash);
 
 event TeeHacked(uint256);
 
@@ -88,6 +79,7 @@ event ChallengeResolved(PendingAssertion);
 contract TeeModule is Ownable {
     // Immutable state variables
     AssertionPoster public immutable poster;
+    IBridge public immutable bridge;
 
     // TEE variables
     TeeTrustedInput public teeTrustedInput;
@@ -99,81 +91,75 @@ contract TeeModule is Ownable {
     uint256 public challengeWindowEnd;
     uint256 public challengeWindowDuration;
 
+    receive() external payable {}
+
     /**
      * @notice Constructs the AssertionPoster contract
      * @param poster_ Address of the assertion poster contract
+     * @param bridge_ Settlement chain address of the appchain `Bridge` contract
      * @param appchainConfigHash_ Hash of the appchain configuration data passed to the TEE
+     * @param seqConfigHash_ Hash of the sequencing chain configuration data passed to the TEE - currently unused
      * Note that the AssertionPoster must be owned by the TeeModule for closing the challenge window to work properly
      */
     constructor(
         AssertionPoster poster_,
+        IBridge bridge_,
         bytes32 appchainConfigHash_,
         bytes32 appchainStartBlockHash_,
-        bytes32 appchainDelayedMessagesHash_,
         bytes32 seqConfigHash_,
         bytes32 seqStartBlockHash_,
-        bytes32 seqDelayedMessagesHash_,
-        uint64 setStartBlockNumber_,
-        uint64 l1StartBlockNumber_
+        bytes32 l1StartBlockHash_,
+        bytes32 teeProgram
     ) {
+        require(l1block.timestamp() > 0 && l1block.hash() > 0, "chain is not an l2 bedrock rollup");
+
+        require(address(poster_).code.length > 0, "poster address does not have any code");
         poster = poster_;
+        require(bridge_.delayedMessageCount() > 0, "insufficient delayed messages in bridge");
+        bridge = bridge_;
 
         // appchain
         teeTrustedInput.appchainConfigHash = appchainConfigHash_;
         teeTrustedInput.appchainStartBlockHash = appchainStartBlockHash_;
-        teeTrustedInput.appchainDelayedMessagesHash = appchainDelayedMessagesHash_;
-        emit TeeAppchainConfigHash(appchainConfigHash_);
+        emit TeeAppchainConfigHash(appchainConfigHash_, appchainStartBlockHash_);
 
         // sequencing chain
         teeTrustedInput.seqConfigHash = seqConfigHash_;
         teeTrustedInput.seqStartBlockHash = seqStartBlockHash_;
-        teeTrustedInput.seqDelayedMessagesHash = seqDelayedMessagesHash_;
-        emit TeeSeqConfigHash(seqConfigHash_);
+        emit TeeSeqConfigHash(seqConfigHash_, seqStartBlockHash_);
 
-        // settlement & l1 chains
-        teeTrustedInput.setStartBlockNumber = setStartBlockNumber_;
-        teeTrustedInput.l1StartBlockNumber = l1StartBlockNumber_;
+        // l1 chain
+        teeTrustedInput.l1StartBlockHash = l1StartBlockHash_;
+
+        require(teeProgram != 0, "tee program hash must be non-zero");
+        teePrograms.push(teeProgram);
+        emit TeeProgramAdded(teeProgram);
 
         closeChallengeWindow();
     }
 
     function closeChallengeWindow() public {
-        IL1Block l1block = IL1Block(address(0x4200000000000000000000000000000000000015));
         require(pendingAssertions.length <= 1, "cannot close challenge window - too many assertions");
         require(
             l1block.timestamp() > challengeWindowEnd, "cannot close challenge window - insufficient time has passed"
         );
         if (pendingAssertions.length > 0) {
             // appchain
-            teeTrustedInput.appchainDelayedMessagesHash = pendingAssertions[0].delayedMessagesHash;
             teeTrustedInput.appchainStartBlockHash = pendingAssertions[0].blockHash;
 
             // sequencing chain
-            teeTrustedInput.seqDelayedMessagesHash = pendingAssertions[0].seqDelayedMessagesHash;
             teeTrustedInput.seqStartBlockHash = pendingAssertions[0].seqBlockHash;
-            teeTrustedInput.setStartBlockNumber = teeTrustedInput.setEndBlockNumber + 1;
 
             // l1 chain
-            teeTrustedInput.l1StartBlockNumber = teeTrustedInput.l1EndBlockNumber + 1;
+            teeTrustedInput.l1StartBlockHash = teeTrustedInput.l1EndBlockHash;
 
             poster.postAssertion(pendingAssertions[0].blockHash, pendingAssertions[0].sendRoot);
         }
 
-        // only process settlement blocks up to the l1 timestamp
-        uint64 delta = (uint64(block.timestamp) - l1block.timestamp()) / 2;
-
-        // blockhash only works for the most recent 256 blocks, use 255 instead of 256 just in case
-        if (delta > 255) {
-            delta = 255;
-        }
-
         // settlement chain
-        teeTrustedInput.setEndBlockNumber = uint64(block.number) - delta;
-        teeTrustedInput.setEndBlockHash = blockhash(teeTrustedInput.setEndBlockNumber);
-        require(teeTrustedInput.setEndBlockHash != 0, "unexpected blockhash of 0");
+        teeTrustedInput.setDelayedMessageAcc = bridge.delayedInboxAccs(bridge.delayedMessageCount() - 1);
 
         // l1 chain
-        teeTrustedInput.l1EndBlockNumber = l1block.number();
         teeTrustedInput.l1EndBlockHash = l1block.hash();
 
         challengeWindowEnd = block.timestamp + challengeWindowDuration;
@@ -201,10 +187,6 @@ contract TeeModule is Ownable {
             (bool success,) = payable(rewardAddr).call{value: address(this).balance}("");
             require(success, "payment failed");
         }
-    }
-
-    function setChallengeWindowDuration(uint256 duration) external onlyOwner {
-        challengeWindowDuration = duration;
     }
 
     function resolveChallenge(PendingAssertion calldata assertion) external onlyOwner {
@@ -250,6 +232,7 @@ contract TeeModule is Ownable {
         emit TeeKeysRevoked();
     }
 
+    // TODO: should this function be removed?
     function addTeeProgram(bytes32 hash) external onlyOwner {
         for (uint256 i = 0; i < teePrograms.length; i++) {
             require(teePrograms[i] != hash, "tee program already exists");
@@ -258,7 +241,9 @@ contract TeeModule is Ownable {
         emit TeeProgramAdded(hash);
     }
 
+    // TODO: should this function be removed?
     function removeTeeProgram(bytes32 hash) public onlyOwner {
+        require(pendingAssertions.length == 0, "cannot remove tee program while assertion is pending");
         for (uint256 i = 0; i < teeProgramKeys[hash].length; i++) {
             isTeeKey[teeProgramKeys[hash][i]] = false;
         }
@@ -266,13 +251,23 @@ contract TeeModule is Ownable {
         emit TeeProgramRemoved(hash);
     }
 
-    function setAppchainConfigHash(bytes32 hash) external onlyOwner {
-        teeTrustedInput.appchainConfigHash = hash;
-        emit TeeAppchainConfigHash(hash);
+    // TODO: should this function be removed?
+    function setChallengeWindowDuration(uint256 duration) external onlyOwner {
+        require(pendingAssertions.length == 0, "cannot update challenge window while assertion is pending");
+        challengeWindowDuration = duration;
     }
 
+    // TODO: should this function be removed?
+    function setAppchainConfigHash(bytes32 hash) external onlyOwner {
+        require(pendingAssertions.length == 0, "cannot update config hash while assertions are pending");
+        teeTrustedInput.appchainConfigHash = hash;
+        emit TeeAppchainConfigHash(hash, teeTrustedInput.appchainStartBlockHash);
+    }
+
+    // TODO: should this function be removed?
     function setSeqConfigHash(bytes32 hash) external onlyOwner {
+        require(pendingAssertions.length == 0, "cannot update config hash while assertions are pending");
         teeTrustedInput.seqConfigHash = hash;
-        emit TeeSeqConfigHash(hash);
+        emit TeeSeqConfigHash(hash, teeTrustedInput.seqStartBlockHash);
     }
 }
