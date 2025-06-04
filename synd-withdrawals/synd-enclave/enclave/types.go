0e039d101181198da84fd7deff6698fab7f20a70
Eric Velazquez <ericvelmar95@gmail.com>
2025-06-04 19:29:42 +0200

    rust<->go types matching

--- synd-withdrawals/synd-enclave/enclave/types.go
+++ synd-withdrawals/synd-enclave/enclave/types.go
@@ -191,3 +191,8 @@ type BlockVerifierInput struct {
 	Messages       []arbostypes.L1IncomingMessage
 	Batch          []byte
 }
+
+
+fn parse_json<T: serde::de::DeserializeOwned>(s: &str) -> Result<T, String> {
+    serde_json::from_str(s).map_err(|e| format!("Invalid JSON: {}", e))
+}
