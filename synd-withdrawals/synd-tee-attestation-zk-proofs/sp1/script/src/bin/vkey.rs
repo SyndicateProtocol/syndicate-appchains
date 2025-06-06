22567a0263f7d682003e4eb038adff1c7a973de9
Jorge Silva <jorge@syndicate.io>
2025-06-06 18:04:00 +0100

    proof submitter CLI application (#529)
    * proof submitter CLI application
    
    * CLI args improvements
    
    * WIP
    
    * improve tests, add custom sp1 contract bindings, re-generate proofs
    
    * update lockfile
    
    * CI fix
    
    * update contract bindings
    
    * lint

--- synd-withdrawals/synd-tee-attestation-zk-proofs/sp1/script/src/bin/vkey.rs
+++ synd-withdrawals/synd-tee-attestation-zk-proofs/sp1/script/src/bin/vkey.rs
@@ -1,10 +1,8 @@
-use sp1_sdk::{include_elf, HashableKey, Prover, ProverClient};
-
-/// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
-pub const X509_ELF: &[u8] = include_elf!("synd-tee-attestation-zk-proofs-sp1-program");
+use sp1_sdk::{HashableKey, Prover, ProverClient};
+use synd_tee_attestation_zk_proofs_sp1_script::shared::TEE_ATTESTATION_VALIDATION_ELF;
 
 fn main() {
     let prover = ProverClient::builder().cpu().build();
-    let (_, vk) = prover.setup(X509_ELF);
+    let (_, vk) = prover.setup(TEE_ATTESTATION_VALIDATION_ELF);
     println!("{}", vk.bytes32());
 }
