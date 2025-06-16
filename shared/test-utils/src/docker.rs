1ddea6a6571a567fed89c2477b1d3984820c2f3d
Eric Velazquez <ericvelmar95@gmail.com>
2025-06-16 11:00:21 +0200

    feat: update enclave Docker to build rust verifiers

--- shared/test-utils/src/docker.rs
+++ shared/test-utils/src/docker.rs
@@ -110,6 +110,7 @@ pub async fn start_component(
     cargs: Vec<String>,
 ) -> Result<Docker> {
     info!("launching {}", executable_name);
+    let needs_rocksdb = executable_name == "synd-mchain";
     let mut docker =
         if let Ok(tag) = env::var(executable_name.to_uppercase().replace("-", "_") + "_TAG") {
             Docker::new(
@@ -122,25 +123,25 @@ pub async fn start_component(
                     .args(args),
             )
         } else {
-            Docker::new(
-                Command::new("cargo")
-                    // ring has a custom build.rs script that rebuilds whenever certain environment
-                    // vars change
-                    .env_remove("CARGO_MANIFEST_DIR")
-                    .env_remove("CARGO_PKG_NAME")
-                    .env_remove("CARGO_PKG_VERSION_MAJOR")
-                    .env_remove("CARGO_PKG_VERSION_MINOR")
-                    .env_remove("CARGO_PKG_VERSION_PATCH")
-                    .env_remove("CARGO_PKG_VERSION_PRE")
-                    .env_remove("CARGO_MANIFEST_LINKS")
-                    .current_dir(env!("CARGO_WORKSPACE_DIR"))
-                    .arg("run")
-                    .arg("--bin")
-                    .arg(executable_name)
-                    .arg("--")
-                    .args(args)
-                    .args(cargs),
-            )
+            let mut cmd = Command::new("cargo");
+            // ring has a custom build.rs script that rebuilds whenever certain environment
+            // vars change
+            cmd.env_remove("CARGO_MANIFEST_DIR")
+                .env_remove("CARGO_PKG_NAME")
+                .env_remove("CARGO_PKG_VERSION_MAJOR")
+                .env_remove("CARGO_PKG_VERSION_MINOR")
+                .env_remove("CARGO_PKG_VERSION_PATCH")
+                .env_remove("CARGO_PKG_VERSION_PRE")
+                .env_remove("CARGO_MANIFEST_LINKS")
+                .current_dir(env!("CARGO_WORKSPACE_DIR"))
+                .arg("run");
+
+            if needs_rocksdb {
+                cmd.arg("--features").arg("rocksdb");
+            }
+
+            cmd.arg("--bin").arg(executable_name).arg("--").args(args).args(cargs);
+            Docker::new(&mut cmd)
         }?;
 
     health_check(executable_name, api_port, &mut docker).await;
