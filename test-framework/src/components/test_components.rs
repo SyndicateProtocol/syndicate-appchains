0012bdc3d71fbc5fa62c20aaa99e4416c555d2b8
Eric Velazquez <ericvelmar95@gmail.com>
2025-06-17 21:06:13 +0200

    fix test components

--- test-framework/src/components/test_components.rs
+++ test-framework/src/components/test_components.rs
@@ -300,7 +300,6 @@ impl TestComponents {
         };
 
         info!("Starting proposer...");
-
         let translator = start_component(
             "synd-translator",
             translator_config.metrics_port,
