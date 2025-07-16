599845585c050ee14b79aff29a5e07fe9ddcd8f4
Eric Velazquez <ericvelmar95@gmail.com>
2025-07-16 14:35:16 -0700

    feat: deploy tee module script

---
+++ synd-contracts/script/DeployTeeModule.s.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+import {Script} from "forge-std/Script.sol";
+import {console2} from "forge-std/console2.sol";
+import {TeeModule} from "src/withdrawal/TeeModule.sol";
+import {AssertionPoster, IRollup} from "src/withdrawal/AssertionPoster.sol";
+import {TeeKeyManager} from "src/withdrawal/TeeKeyManager.sol";
+import {AttestationDocVerifier} from "src/withdrawal/AttestationDocVerifier.sol";
+import {IBridge} from "@arbitrum/nitro-contracts/src/bridge/IBridge.sol";
+
+address constant L1_BLOCK = address(0x4200000000000000000000000000000000000015);
+
+contract DeployTeeModule is Script {
+    function run() public {
+        // Start broadcasting transactions
+        vm.startBroadcast();
+
+        // 1. Deploy the attestation doc verifier if no address provided
+        address attestationDocVerifierAddress = vm.envOr("ATTESTATION_DOC_VERIFIER_ADDRESS", address(0));
+        AttestationDocVerifier attestationDocVerifier;
+        if (attestationDocVerifierAddress != address(0)) {
+            console2.log("Attestation doc verifier already deployed to:", attestationDocVerifierAddress);
+            attestationDocVerifier = AttestationDocVerifier(attestationDocVerifierAddress);
+        } else {
+            console2.log("Deploying attestation doc verifier...");
+            attestationDocVerifier = new AttestationDocVerifier(
+                vm.envAddress("SP1_VERIFIER_ADDRESS"),
+                vm.envBytes32("ATTESTATION_DOC_VERIFIER_V_KEY"),
+                vm.envBytes32("ROOT_CERT_HASH"),
+                keccak256(vm.envBytes("PCR_0")),
+                keccak256(vm.envBytes("PCR_1")),
+                keccak256(vm.envBytes("PCR_2")),
+                keccak256(vm.envBytes("PCR_8")),
+                uint64(vm.envUint("EXPIRATION_TOLERANCE"))
+            );
+            console2.log("Attestation doc verifier deployed to:", address(attestationDocVerifier));
+        }
+        // 2. Deploy the key manager
+        TeeKeyManager keyManager = new TeeKeyManager(attestationDocVerifier);
+
+        // 3. Deploy the assertion poster & configure it
+        IRollup rollup = IRollup(vm.envAddress("ROLLUP_CONTRACT_ADDRESS"));
+        AssertionPoster poster = new AssertionPoster(rollup);
+        poster.configure();
+
+        // 4. Deploy the tee module
+        IBridge bridge = IBridge(vm.envAddress("APPCHAIN_BRIDGE_ADDRESS"));
+        address seqContract = vm.envAddress("SEQUENCING_CONTRACT_ADDRESS");
+        address seqBridge = vm.envAddress("SEQUENCING_BRIDGE_ADDRESS");
+        uint64 setDelay = uint64(vm.envUint("SET_DELAY")); // Usually set to 60s
+        bytes32 appBlockHash = vm.envBytes32("APP_BLOCK_HASH"); // Most recent appchain block hash
+        bytes32 seqBlockHash = vm.envBytes32("SEQ_BLOCK_HASH"); // Sequencing chain block hash that corresponds to the appchain block hash
+        bytes32 l1BatchAcc = vm.envBytes32("L1_BATCH_ACC"); // The sequencing chain start batch accumulator which corresponds to the SEQ_BLOCK_HASH
+        uint64 challengeWindowDuration = uint64(vm.envUint("CHALLENGE_WINDOW_DURATION"));
+        TeeModule teeModule = new TeeModule(
+            poster,
+            bridge,
+            keccak256(abi.encodePacked(seqContract, seqBridge, setDelay)),
+            appBlockHash,
+            seqBlockHash,
+            l1BatchAcc,
+            L1_BLOCK,
+            false,
+            challengeWindowDuration,
+            keyManager
+        );
+        console2.log("TeeModule deployed to:", address(teeModule));
+    }
+}
