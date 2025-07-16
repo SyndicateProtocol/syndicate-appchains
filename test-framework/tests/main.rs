7c1cb056887693b1902a423b0df0465b5696a92d
Daniil Ruditskiy <daniil@syndicate.io>
2025-07-16 09:30:36 -0700

    chore: reduce deps and improve build speed

--- test-framework/tests/main.rs
+++ test-framework/tests/main.rs
@@ -1,8 +1,8 @@
 #![allow(missing_docs)]
 
 // Why define the test files as modules?
-// For files in `/tests`, `cargo` compiles EACH test file into a separate binary.
-// We prefer one main test file with one binary for a faster build.
+// For standalone files in the `/tests` root, `cargo` compiles EACH test file into a separate binary.
+// We prefer one `main.rs` test file with one binary for a faster build.
 // More detail:
 // - https://matklad.github.io/2021/02/27/delete-cargo-integration-tests.html#Loomings-%F0%9F%90%B3,
 // - https://github.com/rust-lang/cargo/pull/5022#issuecomment-364691154
