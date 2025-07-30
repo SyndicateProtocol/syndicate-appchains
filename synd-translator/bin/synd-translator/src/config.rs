91b8c4d2da6c31dd573363139f88c173bd9eebac
Jorge Silva <jorge@syndicate.io>
2025-07-30 12:14:33 +0100

    translator: wait for ingestors to be ready

--- synd-translator/bin/synd-translator/src/config.rs
+++ synd-translator/bin/synd-translator/src/config.rs
@@ -188,9 +188,11 @@ pub struct TranslatorConfig {
     )]
     pub config_manager_address: Option<Address>,
 
+    /// The URL of the Appchain block explorer
     #[arg(long, env = "APPCHAIN_BLOCK_EXPLORER_URL")]
     pub appchain_block_explorer_url: Option<String>,
 
+    /// The timeout for WebSocket requests
     #[arg(long, env = "WS_REQUEST_TIMEOUT", value_parser=humantime::parse_duration, default_value="10s")]
     pub ws_request_timeout: Duration,
 
@@ -202,9 +204,11 @@ pub struct TranslatorConfig {
     #[arg(long, env = "WS_MAX_RESPONSE_SIZE", default_value = "4294967295")] // u32::MAX
     pub max_response_size: u32,
 
+    /// The timeout for eth_getLogs RPC requests
     #[arg(long, env = "GET_LOGS_TIMEOUT", value_parser=humantime::parse_duration, default_value="60s")]
     pub get_logs_timeout: Duration,
 
+    /// The interval to retry RPC requests
     #[arg(
         long,
         env = "RPC_RETRY_INTERVAL",
@@ -212,6 +216,10 @@ pub struct TranslatorConfig {
         value_parser = humantime::parse_duration
     )]
     pub rpc_retry_interval: Duration,
+
+    /// The interval to wait for the ingestors to be ready if not ready yet
+    #[arg(long, env = "WAIT_FOR_INGESTOR_RETRY_INTERVAL", value_parser=humantime::parse_duration, default_value="1s")]
+    pub wait_for_ingestor_retry_interval: Duration,
 }
 
 impl TranslatorConfig {
