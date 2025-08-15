8b72443b412f1225c6348045cb95be10c425c0e3
Daniil Ruditskiy <daniil@syndicate.io>
2025-08-15 09:18:21 -0700

    bug: sequencer nonce too high bug
    - save progress

--- shared/test-utils/src/anvil.rs
+++ shared/test-utils/src/anvil.rs
@@ -10,6 +10,7 @@ use alloy::{
     providers::{ext::AnvilApi as _, Provider, ProviderBuilder},
     rpc::types::{anvil::MineOptions, Block},
 };
+use alloy::primitives::Address;
 use eyre::{eyre, Result};
 use shared::types::FilledProvider;
 
@@ -46,5 +47,11 @@ pub async fn mine_block(provider: &FilledProvider, delay: u64) -> Result<()> {
         .evm_mine(Some(MineOptions::Timestamp(Some(block.header.timestamp + delay))))
         .await
         .unwrap();
+    Ok(())
+}
+
+/// for an Anvil chain, set a wallet's nonce to a provided value
+pub async fn set_address_nonce(provider: &FilledProvider, address: Address, nonce: u64) -> Result<()> {
+    provider.anvil_set_nonce(address, nonce).await?;
     Ok(())
 }
