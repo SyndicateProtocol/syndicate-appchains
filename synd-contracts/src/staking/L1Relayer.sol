be0c938e76bb593e00aca037079b5c1c2a92727b
Isaac <isaac@syndicate.io>
2025-08-21 16:54:29 -0400

    Update relayer with dynamic destination address

--- synd-contracts/src/staking/L1Relayer.sol
+++ synd-contracts/src/staking/L1Relayer.sol
@@ -18,18 +18,14 @@ interface IOPMessageRelayer {
     function sendMessage(address _target, bytes memory _message, uint32 _minGasLimit) external;
 }
 
-interface IL2Relayer {
-    function relay(uint256 amount, uint256 epochIndex) external;
-}
-
 contract L1Relayer {
-    address public opBridge;
-    address public opMessageRelayer;
+    address public immutable opBridge;
+    address public immutable opMessageRelayer;
 
-    address public l1Token;
-    address public l2Token;
+    address public immutable l1Token;
+    address public immutable l2Token;
 
-    address public l2Relayer;
+    address public immutable l2Relayer;
 
     constructor(address _opBridge, address _opMessageRelayer, address _l1Token, address _l2Token, address _l2Relayer) {
         opBridge = _opBridge;
@@ -41,20 +37,20 @@ contract L1Relayer {
         IERC20(l1Token).approve(opBridge, type(uint256).max);
     }
 
-    function relay(uint256 amount, uint256 epochIndex) external {
+    function relay(uint256 amount, address destination, uint256 epochIndex) external {
         require(IERC20(l1Token).balanceOf(address(this)) >= amount, "L1Relayer: Insufficient balance");
 
         _deposit(amount);
-        _relay(amount, epochIndex);
+        _relay(amount, destination, epochIndex);
     }
 
     function _deposit(uint256 amount) internal {
         IOPBridge(opBridge).depositERC20To(l1Token, l2Token, l2Relayer, amount, 200000, bytes(""));
     }
 
-    function _relay(uint256 amount, uint256 epochIndex) internal {
+    function _relay(uint256 amount, address destination, uint256 epochIndex) internal {
         IOPMessageRelayer(opMessageRelayer).sendMessage(
-            l2Relayer, abi.encodeWithSelector(IL2Relayer.relay.selector, amount, epochIndex), 200000
+            l2Relayer, abi.encodeWithSelector(this.relay.selector, amount, destination, epochIndex), 200000
         );
     }
 }
--- synd-contracts/src/staking/L2Relayer.sol
+++ synd-contracts/src/staking/L2Relayer.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.28;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IPool} from "./IPool.sol";
 
 interface IArbBridge {
     function depositERC20(uint256 amount) external returns (uint256);
@@ -15,39 +16,32 @@ interface IArbBridge {
     ) external returns (uint256);
 }
 
-interface IPool {
-    function deposit(uint256 epochIndex) external;
-}
-
 contract L2Relayer {
-    address public arbBridge;
-    address public tokenAddress;
-    address public poolDestination;
-
-    constructor(address _arbBridge, address _tokenAddress, address _poolDestination) {
+    address public immutable arbBridge;
+ 
+    constructor(address _arbBridge, address _tokenAddress) {
         arbBridge = _arbBridge;
         tokenAddress = _tokenAddress;
-        poolDestination = _poolDestination;
 
         IERC20(tokenAddress).approve(arbBridge, type(uint256).max);
     }
 
-    function relay(uint256 amount, uint256 epochIndex) external {
+    function relay(uint256 amount, address destination, uint256 epochIndex) external {
         require(IERC20(tokenAddress).balanceOf(address(this)) >= amount, "L2Relayer: Insufficient balance");
 
         _deposit(amount);
-        _relay(amount, epochIndex);
+        _relay(amount, destination, epochIndex);
     }
 
     function _deposit(uint256 amount) internal {
         IArbBridge(arbBridge).depositERC20(amount);
     }
 
-    function _relay(uint256 amount, uint256 epochIndex) internal {
+    function _relay(uint256 amount, address destination, uint256 epochIndex) internal {
         uint256 gasLimit = 210000; // 21k gas for transfer x 10
-        uint256 maxFeePerGas = 1000000000; // 1 gwei
+        uint256 maxFeePerGas = 1 gwei;
         IArbBridge(arbBridge).sendContractTransaction(
-            gasLimit, maxFeePerGas, poolDestination, amount, abi.encodeWithSelector(IPool.deposit.selector, epochIndex)
+            gasLimit, maxFeePerGas, destination, amount, abi.encodeWithSelector(IPool.deposit.selector, epochIndex)
         );
     }
 }
