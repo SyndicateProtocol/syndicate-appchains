ccb5ed137e6566f007bddf4d90236be9f6eadc04
Daniil Ruditskiy <daniil@syndicate.io>
2025-08-28 15:27:37 -0700

    fix: prover from env

--- synd-withdrawals/synd-tee-attestation-zk-proofs/sp1/script/src/bin/vkey.rs
+++ synd-withdrawals/synd-tee-attestation-zk-proofs/sp1/script/src/bin/vkey.rs
@@ -2,7 +2,7 @@ use sp1_sdk::{HashableKey, Prover, ProverClient};
 use synd_tee_attestation_zk_proofs_sp1_script::shared::TEE_ATTESTATION_VALIDATION_ELF;
 
 fn main() {
-    let cuda_prover = ProverClient::from_env();
-    let (_, vk) = cuda_prover.setup(TEE_ATTESTATION_VALIDATION_ELF);
+    let prover = ProverClient::from_env();
+    let (_, vk) = prover.setup(TEE_ATTESTATION_VALIDATION_ELF);
     println!("{}", vk.bytes32());
 }
