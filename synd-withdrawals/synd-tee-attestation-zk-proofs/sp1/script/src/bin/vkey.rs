4ba0661407678c3ecea856646f91023506f4b24d
Daniil Ruditskiy <daniil@syndicate.io>
2025-08-28 15:24:55 -0700

    fix: cuda prover

--- synd-withdrawals/synd-tee-attestation-zk-proofs/sp1/script/src/bin/vkey.rs
+++ synd-withdrawals/synd-tee-attestation-zk-proofs/sp1/script/src/bin/vkey.rs
@@ -2,7 +2,8 @@ use sp1_sdk::{HashableKey, Prover, ProverClient};
 use synd_tee_attestation_zk_proofs_sp1_script::shared::TEE_ATTESTATION_VALIDATION_ELF;
 
 fn main() {
-    let prover = ProverClient::builder().cpu().build();
-    let (_, vk) = prover.setup(TEE_ATTESTATION_VALIDATION_ELF);
+    let _cpu_prover = ProverClient::builder().cpu().build();
+    let cuda_prover = ProverClient::builder().cuda().build();
+    let (_, vk) = cuda_prover.setup(TEE_ATTESTATION_VALIDATION_ELF);
     println!("{}", vk.bytes32());
 }
