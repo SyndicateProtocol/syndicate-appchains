d6695d98ece5765fc9b1d8faa0206caa4610d487
Eric Velazquez <ericvelmar95@gmail.com>
2025-08-29 13:37:08 -0700

    bindings

--- shared/contract-bindings/src/synd/mod.rs
+++ shared/contract-bindings/src/synd/mod.rs
@@ -206,6 +206,7 @@ pub mod r#stack_frame_lib;
 pub mod r#storage_slot;
 pub mod r#strings;
 pub mod r#synd_staking;
+pub mod r#reentrant_contract;
 pub mod r#syndicate_factory;
 pub mod r#syndicate_factory_wrapper;
 pub mod r#syndicate_sequencing_chain;
