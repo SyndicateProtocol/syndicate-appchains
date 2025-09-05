b00c7d32b977df53a3a85330d1a78f59cfa85ca1
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2025-09-05 18:11:13 +0200

    fix lint

--- synd-contracts/test/upgrade/StorageUpgradeTest.t.sol
+++ synd-contracts/test/upgrade/StorageUpgradeTest.t.sol
@@ -5,7 +5,8 @@ import {Test} from "forge-std/Test.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 import {SyndicateSequencingChain} from "../../src/SyndicateSequencingChain.sol";
-import {SyndicateSequencingChainTestingUpgradeability} from "./helpers/SyndicateSequencingChainTestingUpgradeability.sol";
+import {SyndicateSequencingChainTestingUpgradeability} from
+    "./helpers/SyndicateSequencingChainTestingUpgradeability.sol";
 import {IPermissionModule} from "../../src/interfaces/IPermissionModule.sol";
 
 /// @notice Mock factory contract for testing upgrades
