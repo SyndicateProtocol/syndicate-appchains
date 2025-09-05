c596810bfbe587e256ef08e2b77ccb4bccffcf00
Eric Velazquez <ericvelmar95@gmail.com>
2025-09-05 14:54:24 -0700

    feat: appchain pool vesting schedule (#785)
    * feat: appchain pool vesting schedule
    
    * feat: appchain pool vesting schedule
    
    * update tests and bindings
    
    * rm comments
    
    * pr review
    
    * pr review
    
    * bindings
    
    * bindings

--- synd-contracts/src/staking/L2Relayer.sol
+++ synd-contracts/src/staking/L2Relayer.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.28;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
-import {IPool} from "./IPool.sol";
+import {IPool} from "./interfaces/IPool.sol";
 
 interface IArbBridge {
     function unsafeCreateRetryableTicket(
