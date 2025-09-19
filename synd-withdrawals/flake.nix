4959691aff29fc7bb6ff43ec12763de14b1a33eb
tsite <stubydent@gmail.com>
2025-09-19 11:21:59 -0700

    use reproducible builds for eif.bin (#840)
    Ticket
    Related Linear Ticket: SEQ-1383
    What does this PR do?
    Summary:
    add nix reproducible builds for eif.bin to the ci
    Key Changes:
    update the dockerfile to build the enclave image using nix
    update ci to use a docker image for running the proposer instead of compiling it outside of docker
    reduce submodule usage
    Breaking changes?
    Yes/No:
    No
    Metrics changes?
    Yes/No:
    No
    How can this PR be tested?
    ci tests

--- synd-withdrawals/flake.nix
+++ synd-withdrawals/flake.nix
@@ -9,15 +9,23 @@
     systems.url = "github:nix-systems/default-linux";
     nix-filter.url = "github:numtide/nix-filter";
 
-    solc = {
-      url = "github:hellwolf/solc.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     nitro = {
       type = "git";
       url = "https://github.com/SyndicateProtocol/nitro.git";
+      rev = "2625cbb20485326af9f8c05bce6b025b42ba59b6";
       submodules = true;
+      flake = false;
+    };
+
+    solc-0-8-17 = {
+      type = "file";
+      url = "https://binaries.soliditylang.org/emscripten-wasm32/solc-emscripten-wasm32-v0.8.17+commit.8df45f5f.js";
+      flake = false;
+    };
+
+    solc-0-8-24 = {
+      type = "file";
+      url = "https://binaries.soliditylang.org/emscripten-wasm32/solc-emscripten-wasm32-v0.8.24+commit.e11b9ed9.js";
       flake = false;
     };
 
@@ -25,7 +33,7 @@
       type = "github";
       owner = "aws";
       repo = "aws-nitro-enclaves-sdk-bootstrap";
-      rev = "7614f19963e4e956493b3260fda4d62834bb281c";
+      rev = "f718dea60a9d9bb8b8682fd852ad793912f3c5db";
       flake = false;
     };
 
