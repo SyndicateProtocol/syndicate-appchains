f66b669a3c0a10fac4390c4408740cafde0ad593
Gus <gustavoguimaraes@users.noreply.github.com>
2025-10-24 10:24:40 +0200

    change SPDX to MIT License (#873)

--- synd-contracts/src/token/testnet-synd-token/TestnetSyndTokenCrosschain.sol
+++ synd-contracts/src/token/testnet-synd-token/TestnetSyndTokenCrosschain.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
 import {SyndicateTokenCrosschain, IBridgeRateLimiter, IERC7802} from "../SyndicateTokenCrosschain.sol";
