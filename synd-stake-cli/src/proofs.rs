635efcc30482e920275a19cf12caad4e551d097b
Isaac <isaac@syndicate.io>
2025-10-27 11:44:02 -0400

    fix cli

--- synd-stake-cli/src/proofs.rs
+++ synd-stake-cli/src/proofs.rs
@@ -24,6 +24,7 @@ use url::Url;
 
 // Legacy interface for Sequencing Chain
 sol! {
+    #[sol(rpc)]
     contract ILegacySequencingChain {
         function getTokensForEpoch(uint256 epoch) external view returns (uint256);
     }
