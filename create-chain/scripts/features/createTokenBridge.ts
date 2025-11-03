a7c30ff8781ec1aae1e5c63d4fa297f51ea00e78
caleb <caleb@syndicate.io>
2025-11-03 14:40:13 -0600

    format createTokenBridge

--- create-chain/scripts/features/createTokenBridge.ts
+++ create-chain/scripts/features/createTokenBridge.ts
@@ -1,10 +1,23 @@
-import { createTokenBridgeEnoughCustomFeeTokenAllowance, CreateTokenBridgeEnoughCustomFeeTokenAllowanceParams, CreateTokenBridgeParams, createTokenBridgePrepareCustomFeeTokenApprovalTransactionRequest, createTokenBridgePrepareSetWethGatewayTransactionReceipt, createTokenBridgePrepareSetWethGatewayTransactionRequest, createTokenBridgePrepareTransactionReceipt, createTokenBridgePrepareTransactionRequest, isTokenBridgeDeployed } from "@arbitrum/orbit-sdk";
-import { Chain, zeroAddress } from "viem";
-import { getChainExplorerUrl } from "../utils/helpers";
-import { print } from "../utils/print";
+import {
+  createTokenBridgeEnoughCustomFeeTokenAllowance,
+  CreateTokenBridgeEnoughCustomFeeTokenAllowanceParams,
+  CreateTokenBridgeParams,
+  createTokenBridgePrepareCustomFeeTokenApprovalTransactionRequest,
+  createTokenBridgePrepareSetWethGatewayTransactionReceipt,
+  createTokenBridgePrepareSetWethGatewayTransactionRequest,
+  createTokenBridgePrepareTransactionReceipt,
+  createTokenBridgePrepareTransactionRequest,
+  isTokenBridgeDeployed
+} from "@arbitrum/orbit-sdk"
+import { Chain, zeroAddress } from "viem"
+import { getChainExplorerUrl } from "../utils/helpers"
+import { print } from "../utils/print"
 
 // Source: https://github.com/OffchainLabs/arbitrum-orbit-sdk/blob/7143a874a94dc0d59d076a0407319f4927f5f49d/src/createTokenBridge.ts#L171-L172
-export async function createTokenBridge<TParentChain extends Chain | undefined, TOrbitChain extends Chain | undefined>({
+export async function createTokenBridge<
+  TParentChain extends Chain | undefined,
+  TOrbitChain extends Chain | undefined
+>({
   rollupOwner,
   rollupAddress,
   rollupDeploymentBlockNumber,
@@ -15,50 +28,59 @@ export async function createTokenBridge<TParentChain extends Chain | undefined, 
   tokenBridgeCreatorAddressOverride,
   gasOverrides,
   retryableGasOverrides,
-  setWethGatewayGasOverrides,
+  setWethGatewayGasOverrides
 }: CreateTokenBridgeParams<TParentChain, TOrbitChain>) {
-    const isTokenBridgeAlreadyDeployed = await isTokenBridgeDeployed({
-      parentChainPublicClient,
-      orbitChainPublicClient,
-      rollup: rollupAddress,
-      tokenBridgeCreatorAddressOverride,
-    });
+  const isTokenBridgeAlreadyDeployed = await isTokenBridgeDeployed({
+    parentChainPublicClient,
+    orbitChainPublicClient,
+    rollup: rollupAddress,
+    tokenBridgeCreatorAddressOverride
+  })
 
-    if (isTokenBridgeAlreadyDeployed) {
-      throw new Error(`Token bridge contracts for Rollup ${rollupAddress} are already deployed`);
-    }
+  if (isTokenBridgeAlreadyDeployed) {
+    throw new Error(
+      `Token bridge contracts for Rollup ${rollupAddress} are already deployed`
+    )
+  }
 
-    const isCustomFeeTokenBridge = nativeTokenAddress && nativeTokenAddress !== zeroAddress;
-      if (isCustomFeeTokenBridge) {
+  const isCustomFeeTokenBridge =
+    nativeTokenAddress && nativeTokenAddress !== zeroAddress
+  if (isCustomFeeTokenBridge) {
     // set the custom fee token
     // prepare transaction to approve custom fee token spend
-    const allowanceParams: CreateTokenBridgeEnoughCustomFeeTokenAllowanceParams<TParentChain> = {
-      nativeToken: nativeTokenAddress,
-      owner: account.address,
-      publicClient: parentChainPublicClient,
-      tokenBridgeCreatorAddressOverride,
-    };
+    const allowanceParams: CreateTokenBridgeEnoughCustomFeeTokenAllowanceParams<TParentChain> =
+      {
+        nativeToken: nativeTokenAddress,
+        owner: account.address,
+        publicClient: parentChainPublicClient,
+        tokenBridgeCreatorAddressOverride
+      }
 
     // Check allowance and approve if necessary
-    if (!(await createTokenBridgeEnoughCustomFeeTokenAllowance(allowanceParams))) {
+    if (
+      !(await createTokenBridgeEnoughCustomFeeTokenAllowance(allowanceParams))
+    ) {
       const approvalTxRequest =
-        await createTokenBridgePrepareCustomFeeTokenApprovalTransactionRequest(allowanceParams);
+        await createTokenBridgePrepareCustomFeeTokenApprovalTransactionRequest(
+          allowanceParams
+        )
 
       // sign and send the transaction
       const approvalTxHash = await parentChainPublicClient.sendRawTransaction({
-        serializedTransaction: await account.signTransaction(approvalTxRequest),
-      });
+        serializedTransaction: await account.signTransaction(approvalTxRequest)
+      })
 
       // get the transaction receipt after waiting for the transaction to complete
-      const approvalTxReceipt = await parentChainPublicClient.waitForTransactionReceipt({
-        hash: approvalTxHash,
-      });
+      const approvalTxReceipt =
+        await parentChainPublicClient.waitForTransactionReceipt({
+          hash: approvalTxHash
+        })
 
       print(
         `Tokens approved in ${getChainExplorerUrl(parentChainPublicClient.chain!)}/tx/${
           approvalTxReceipt.transactionHash
-        }`,
-      );
+        }`
+      )
     }
   }
 
@@ -66,105 +88,115 @@ export async function createTokenBridge<TParentChain extends Chain | undefined, 
   const txRequest = await createTokenBridgePrepareTransactionRequest({
     params: {
       rollup: rollupAddress,
-      rollupOwner,
+      rollupOwner
     },
     parentChainPublicClient,
     orbitChainPublicClient,
     account: account.address,
     tokenBridgeCreatorAddressOverride,
     gasOverrides,
-    retryableGasOverrides,
-  });
+    retryableGasOverrides
+  })
 
   // sign and send the transaction
-  print(`Deploying the TokenBridge...`);
+  print(`Deploying the TokenBridge...`)
   const txHash = await parentChainPublicClient.sendRawTransaction({
-    serializedTransaction: await account.signTransaction(txRequest),
-  });
+    serializedTransaction: await account.signTransaction(txRequest)
+  })
 
   // wait for the transaction to be mined
-  const receipt = await parentChainPublicClient.waitForTransactionReceipt({ hash: txHash });
+  const receipt = await parentChainPublicClient.waitForTransactionReceipt({
+    hash: txHash
+  })
 
-  const transaction = await parentChainPublicClient.getTransaction({ hash: receipt.transactionHash });
+  const transaction = await parentChainPublicClient.getTransaction({
+    hash: receipt.transactionHash
+  })
 
   // get the transaction receipt after waiting for the transaction to complete
-  const txReceipt = createTokenBridgePrepareTransactionReceipt(
-    receipt,
-  );
+  const txReceipt = createTokenBridgePrepareTransactionReceipt(receipt)
   print(
     `Deployed in ${getChainExplorerUrl(parentChainPublicClient.chain!)}/tx/${
       txReceipt.transactionHash
-    }`,
-  );
+    }`
+  )
 
   // wait for retryables to execute
-  print(`Waiting for retryable tickets to execute on the Orbit chain...`);
+  print(`Waiting for retryable tickets to execute on the Orbit chain...`)
   const orbitChainRetryableReceipts = await txReceipt.waitForRetryables({
     // @ts-ignore (todo: fix viem type issue)
-    orbitPublicClient: orbitChainPublicClient,
-  });
-  print(`Retryables executed`);
+    orbitPublicClient: orbitChainPublicClient
+  })
+  print(`Retryables executed`)
   print(
-    `Transaction hash for first retryable is ${orbitChainRetryableReceipts[0].transactionHash}`,
-  );
+    `Transaction hash for first retryable is ${orbitChainRetryableReceipts[0].transactionHash}`
+  )
   print(
-    `Transaction hash for second retryable is ${orbitChainRetryableReceipts[1].transactionHash}`,
-  );
+    `Transaction hash for second retryable is ${orbitChainRetryableReceipts[1].transactionHash}`
+  )
 
   // fetching the TokenBridge contracts
   const tokenBridgeContracts = await txReceipt.getTokenBridgeContracts({
     // @ts-ignore (todo: fix viem type issue)
-    parentChainPublicClient,
-  });
+    parentChainPublicClient
+  })
 
   // Non custom fee token
   if (!isCustomFeeTokenBridge) {
     // set weth gateway
-    const setWethGatewayTxRequest = await createTokenBridgePrepareSetWethGatewayTransactionRequest({
-      rollup: rollupAddress,
-      rollupDeploymentBlockNumber,
-      parentChainPublicClient,
-      orbitChainPublicClient,
-      account: account.address,
-      tokenBridgeCreatorAddressOverride,
-      retryableGasOverrides: setWethGatewayGasOverrides,
-    });
+    const setWethGatewayTxRequest =
+      await createTokenBridgePrepareSetWethGatewayTransactionRequest({
+        rollup: rollupAddress,
+        rollupDeploymentBlockNumber,
+        parentChainPublicClient,
+        orbitChainPublicClient,
+        account: account.address,
+        tokenBridgeCreatorAddressOverride,
+        retryableGasOverrides: setWethGatewayGasOverrides
+      })
 
     // sign and send the transaction
-    const setWethGatewayTxHash = await parentChainPublicClient.sendRawTransaction({
-      serializedTransaction: await account.signTransaction(setWethGatewayTxRequest),
-    });
+    const setWethGatewayTxHash =
+      await parentChainPublicClient.sendRawTransaction({
+        serializedTransaction: await account.signTransaction(
+          setWethGatewayTxRequest
+        )
+      })
 
-    const setWethGatewayTransaction = await parentChainPublicClient.getTransaction({
-      hash: setWethGatewayTxHash,
-    });
+    const setWethGatewayTransaction =
+      await parentChainPublicClient.getTransaction({
+        hash: setWethGatewayTxHash
+      })
 
     // get the transaction receipt after waiting for the transaction to complete
-    const setWethGatewayTxReceipt = createTokenBridgePrepareSetWethGatewayTransactionReceipt(
-      await parentChainPublicClient.waitForTransactionReceipt({ hash: setWethGatewayTxHash }),
-    );
+    const setWethGatewayTxReceipt =
+      createTokenBridgePrepareSetWethGatewayTransactionReceipt(
+        await parentChainPublicClient.waitForTransactionReceipt({
+          hash: setWethGatewayTxHash
+        })
+      )
 
     print(
       `Weth gateway set in ${getChainExplorerUrl(parentChainPublicClient.chain!)}/tx/${
         setWethGatewayTxReceipt.transactionHash
-      }`,
-    );
+      }`
+    )
 
     // Wait for retryables to execute
     const orbitChainSetWethGatewayRetryableReceipt =
       await setWethGatewayTxReceipt.waitForRetryables({
         // @ts-ignore (todo: fix viem type issue)
-        orbitPublicClient: orbitChainPublicClient,
-      });
-    print(`Retryables executed`);
+        orbitPublicClient: orbitChainPublicClient
+      })
+    print(`Retryables executed`)
     print(
-      `Transaction hash for retryable is ${orbitChainSetWethGatewayRetryableReceipt[0].transactionHash}`,
-    );
+      `Transaction hash for retryable is ${orbitChainSetWethGatewayRetryableReceipt[0].transactionHash}`
+    )
 
-    if (orbitChainSetWethGatewayRetryableReceipt[0].status !== 'success') {
+    if (orbitChainSetWethGatewayRetryableReceipt[0].status !== "success") {
       throw new Error(
-        `Retryable status is not success: ${orbitChainSetWethGatewayRetryableReceipt[0].status}. Aborting...`,
-      );
+        `Retryable status is not success: ${orbitChainSetWethGatewayRetryableReceipt[0].status}. Aborting...`
+      )
     }
 
     return {
@@ -175,15 +207,15 @@ export async function createTokenBridge<TParentChain extends Chain | undefined, 
       setWethGateway: {
         transaction: setWethGatewayTransaction,
         transactionReceipt: setWethGatewayTxReceipt,
-        retryables: [orbitChainSetWethGatewayRetryableReceipt[0]],
-      },
-    };
+        retryables: [orbitChainSetWethGatewayRetryableReceipt[0]]
+      }
+    }
   }
 
   return {
     transaction,
     transactionReceipt: txReceipt,
     retryables: orbitChainRetryableReceipts,
-    tokenBridgeContracts,
-  };
+    tokenBridgeContracts
+  }
 }
